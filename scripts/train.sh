f1cd901823956a9fa5a475c21e34a0a18b6d2ad3
DimensionSTP <ddang8jh@gmail.com>
2024-11-13 15:29:08 +0900

    feat: default scripts

--- scripts/train.sh
+++ scripts/train.sh
@@ -2,90 +2,54 @@
 
 is_tuned="untuned"
 strategy="ddp"
-image_upload_user="microsoft"
-image_model_type="dit-large-finetuned-rvlcdip"
-text_upload_user="klue"
-text_model_type="roberta-large"
+model_type="swin_large_patch4_window7_224"
+pretrained="pretrained"
+is_crop=True
+classification_types="0 1 2 3 4"
 precision=32
-batch_size=16
+batch_size=32
+workers_ratio=8
+use_all_workers=False
 
-python main.py --config-name=multimodal.yaml \
-    mode=train \
-    is_tuned=$is_tuned \
-    strategy=$strategy \
-    image_upload_user=$image_upload_user \
-    image_model_type=$image_model_type \
-    text_upload_user=$text_upload_user \
-    text_model_type=$text_model_type \
-    precision=$precision \
-    batch_size=$batch_size
+for classification_type in $classification_types
+do
+    python main.py --config-name=timm.yaml \
+        mode=train \
+        is_tuned=$is_tuned \
+        strategy=$strategy \
+        model_type=$model_type \
+        pretrained=$pretrained \
+        is_crop=$is_crop \
+        classification_type=$classification_type \
+        precision=$precision \
+        batch_size=$batch_size \
+        workers_ratio=$workers_ratio \
+        use_all_workers=$use_all_workers
+done
 
-modality="text"
-is_tuned="untuned"
-strategy="ddp"
-upload_user="klue"
-model_type="roberta-large"
-precision=32
-batch_size=24
-
-python main.py --config-name=huggingface.yaml \
-    mode=train \
-    modality=$modality \
-    is_tuned=$is_tuned \
-    strategy=$strategy \
-    upload_user=$upload_user \
-    model_type=$model_type \
-    precision=$precision \
-    batch_size=$batch_size
-
-modality="image"
 is_tuned="untuned"
 strategy="ddp"
 upload_user="microsoft"
-model_type="dit-large-finetuned-rvlcdip"
+model_type="swin-large-patch4-window7-224-in22k"
+is_crop=True
+classification_types="0 1 2 3 4"
 precision=32
-batch_size=24
+batch_size=32
+workers_ratio=8
+use_all_workers=False
 
-python main.py --config-name=huggingface.yaml \
-    mode=train \
-    modality=$modality \
-    is_tuned=$is_tuned \
-    strategy=$strategy \
-    upload_user=$upload_user \
-    model_type=$model_type \
-    precision=$precision \
-    batch_size=$batch_size
-
-modality="multimodal"
-is_tuned="untuned"
-strategy="ddp"
-upload_user="nielsr"
-model_type="layoutlmv3-finetuned-cord"
-precision=32
-batch_size=24
-
-python main.py --config-name=huggingface.yaml \
-    mode=train \
-    modality=$modality \
-    is_tuned=$is_tuned \
-    strategy=$strategy \
-    upload_user=$upload_user \
-    model_type=$model_type \
-    precision=$precision \
-    batch_size=$batch_size
-
-is_tuned="untuned"
-strategy="ddp"
-model_type="efficientnet_b0.ra_in1k"
-pretrained="pretrained"
-precision=32
-batch_size=24
-
-python main.py --config-name=timm.yaml \
-    mode=train \
-    is_tuned=$is_tuned \
-    strategy=$strategy \
-    model_type=$model_type \
-    pretrained=$pretrained \
-    precision=$precision \
-    batch_size=$batch_size
+for classification_type in $classification_types
+do
+    python main.py --config-name=huggingface.yaml \
+        mode=train \
+        is_tuned=$is_tuned \
+        strategy=$strategy \
+        upload_user=$upload_user \
+        model_type=$model_type \
+        is_crop=$is_crop \
+        classification_type=$classification_type \
+        precision=$precision \
+        batch_size=$batch_size \
+        workers_ratio=$workers_ratio \
+        use_all_workers=$use_all_workers
+done
