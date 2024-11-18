409c9164cfbc98508948647aeedd9ad2149a2899
DimensionSTP <ddang8jh@gmail.com>
2024-11-19 01:14:23 +0900

    refactor: dataset -> metadata

--- src/preprocessing/split_metadata.py
+++ src/preprocessing/split_metadata.py
@@ -18,22 +18,22 @@ from omegaconf import DictConfig
 def split_metadata(
     config: DictConfig,
 ) -> None:
-    dataset = pd.read_csv(f"{config.connected_dir}/metadata/metadata.csv")
-    train_dataset, test_dataset = train_test_split(
-        dataset,
+    metadata = pd.read_csv(f"{config.connected_dir}/metadata/metadata.csv")
+    train_metadata, test_metadata = train_test_split(
+        metadata,
         test_size=config.split_ratio,
         random_state=config.seed,
         shuffle=True,
-        stratify=dataset[config.target_column_name],
+        stratify=metadata[config.target_column_name],
     )
-    train_dataset = train_dataset.reset_index(drop=True)
-    test_dataset = test_dataset.reset_index(drop=True)
+    train_metadata = train_metadata.reset_index(drop=True)
+    test_metadata = test_metadata.reset_index(drop=True)
 
-    train_dataset.to_csv(
+    train_metadata.to_csv(
         f"{config.connected_dir}/metadata/train.csv",
         index=False,
     )
-    test_dataset.to_csv(
+    test_metadata.to_csv(
         f"{config.connected_dir}/metadata/test.csv",
         index=False,
     )
